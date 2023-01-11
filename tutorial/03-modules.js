003e045d951ffd54eb3ca784ba81a94da41cb59c
Mỹ Nguyễn <mynguyen@everstar.vn>
2023-01-11 19:24:55 +0700

    first init

---
+++ tutorial/03-modules.js
@@ -0,0 +1,8 @@
+//Modules
+const { john } = require('./04-names')
+const sayHi = require('./05-utils')
+const data = require('./06-alternative-flavor')
+require('./07-mind-grenade')
+
+sayHi('susan')
+sayHi(john)
---
+++ tutorial/1-intro.js
@@ -0,0 +1,8 @@
+const amount = 14
+
+if (amount < 10) {
+  console.log('small number')
+} else {
+  console.log('larger number')
+}
+console.log('this is my first node app!!!')
