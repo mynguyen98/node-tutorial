847f68dbd8d0df7aba5b362890a3a491005be2b6
Mỹ Nguyễn <mynguyen@everstar.vn>
2023-01-12 20:02:28 +0700

    12

---
+++ tutorial/13-HTTP-module.js
@@ -0,0 +1,16 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.end('Welcome to our home page')
+    return
+  }
+  if (req.url === '/about') {
+    res.end('Here is our short history')
+    return
+  }
+  res.end(`
+  <h1>Oops!, we cant seem to find the page you are looking for</h1>`)
+})
+
+server.listen(5000)
